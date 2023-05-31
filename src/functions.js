d4b85050c4f1b1ee714ce552426b7702f8d83586
arthur.lecompte <arthur.lecompte14@gmail.com>
2023-05-31 09:07:34 +0200

    Students filters

--- src/functions.js
+++ src/functions.js
@@ -98,6 +98,121 @@ const findStudentsWithoutCiImage = async (organism) => {
 	return students;
 }
 
+const findStudents = async (params = {}) => {
+	let filters = {};
+	if (params.hasOwnProperty('organism')) {
+		if (Array.isArray(params.organism)) {
+			filters.organism = {
+				$in: params.organism.map((organism) => {
+					return {
+						"$oid": organism
+					}
+				})
+			}
+		} else {
+			filters.organism = {
+				$eq: {
+					"$oid": params.organism
+				}
+			}
+		}
+	}
+
+	if (params.hasOwnProperty('promotion')) {
+		//check if there is multiple promotions
+		if (Array.isArray(params.promotion)) {
+			filters.promotion = {
+				$in: params.promotion.map((promotion) => {
+					return {
+						"$oid": promotion
+					}
+				})
+			}
+		} else {
+			filters.promotion = {
+				$eq: {
+					"$oid": params.promotion
+				}
+			}
+		}
+	}
+
+	if (params.hasOwnProperty('degree')) {
+		//check if there is multiple degrees
+		if (Array.isArray(params.degree)) {
+			filters.degree = {
+				$in: params.degree.map((degree) => {
+					return {
+						"$oid": degree
+					}
+				})
+			}
+		} else {
+			filters.degree = {
+				$eq: {
+					"$oid": params.degree
+				}
+			}
+		}
+	}
+
+	if (params.hasOwnProperty('search')) {
+
+	}
+
+
+	return await sendData({
+		pipeline: [
+			{
+				$match: filters
+			},
+			{
+				$lookup: {
+					from: "organisms",
+					localField: "organism",
+					foreignField: "_id",
+					as: "organism"
+				},
+			},
+			{
+				$unwind: {
+					path: '$organism',
+					preserveNullAndEmptyArrays: true
+				}
+			},
+			{
+				$lookup: {
+					from: "promotions",
+					localField: "promotion",
+					foreignField: "_id",
+					as: "promotion"
+				}
+			},
+			{
+				$unwind: {
+					path: '$promotion',
+					preserveNullAndEmptyArrays: true
+				}
+			},
+			{
+				$lookup: {
+					from: "degrees",
+					localField: "promotion.degree",
+					foreignField: "_id",
+					as: "degree"
+				}
+			},
+			{
+				$unwind: {
+					path: '$degree',
+					preserveNullAndEmptyArrays: true
+				}
+			}
+		]
+	}, 'students', 'aggregate', {});
+
+}
+
 module.exports = {
 	findStudentsWithoutNumeroCi,
 	findStudentsWithoutCiImage
